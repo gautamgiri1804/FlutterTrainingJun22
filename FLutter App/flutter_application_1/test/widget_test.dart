5be3d642b690629858089a3a4590911d0dbd7a5f
pankkap <pankkap@gmail.com>
2022-06-11 15:25:58 +0530

    Day-10 Code

--- FLutter App/flutter_application_1/test/widget_test.dart
+++ FLutter App/flutter_application_1/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_application_1/2-Scaffold.dart';
+import 'package:flutter_application_1/2-ScaffoldWidget.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
